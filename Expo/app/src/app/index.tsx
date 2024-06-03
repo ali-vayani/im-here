00e3a3492943448ecc7de04efb69580f4a1fa82a
pAsta-kun <ila.00123ivay@gmail.com>
2024-06-03 13:56:49 -0500

    login page frontend made

--- Expo/app/src/app/index.tsx
+++ Expo/app/src/app/index.tsx
@@ -1,95 +1,46 @@
 import { Link } from "expo-router";
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet, TextInput, Image, TouchableOpacity } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Page() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
   return (
-    <View className="flex flex-1">
-      <Header />
-      <Content />
-      <Footer />
-    </View>
-  );
-}
+    <View className="flex flex-1 bg-background items-center justify-start">
+      <View className="flex h-1/3 w-full items-center justify-end">
+        <Text className="text-5xl font-semibold">I'm Here</Text>
+      </View>
 
-function Content() {
-  return (
-    <View className="flex-1">
-      <View className="py-12 md:py-24 lg:py-32 xl:py-48">
-        <View className="px-4 md:px-6">
-          <View className="flex flex-col items-center gap-4 text-center">
-            <Text
-              role="heading"
-              className="text-3xl text-center native:text-5xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl"
-            >
-              Welcome to Project ACME
-            </Text>
-            <Text className="mx-auto max-w-[700px] text-lg text-center text-gray-500 md:text-xl dark:text-gray-400">
-              Discover and collaborate on amce. Explore our services now.
-            </Text>
+      <View className="flex h-1/3 w-full items-center justify-center">
+        <TextInput placeholder="Phone number or username" onChangeText={(text) => setUsername(text)} value={username} placeholderTextColor={'rgba(6, 3, 31)'} className="bg-text/10 w-11/12 h-16 px-4 my-2 rounded-md border-2 border-text/50"/>
+        <TextInput placeholder="Password" onChangeText={(text) => setPassword(text)} value={password} placeholderTextColor={'rgba(6, 3, 31)'} className="bg-text/10 w-11/12 h-16 px-4 my-2 rounded-md border-2 border-text/50"/>
 
-            <View className="gap-4">
-              <Link
-                suppressHighlighting
-                className="flex h-9 items-center justify-center overflow-hidden rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 web:shadow ios:shadow transition-colors hover:bg-gray-900/90 active:bg-gray-400/90 web:focus-visible:outline-none web:focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                href="/"
-              >
-                Explore
-              </Link>
-            </View>
-          </View>
-        </View>
+        <TouchableOpacity onPress={() => resetPassword(username, password)}>
+          <Text className="text-primary">Forgot Password</Text>
+        </TouchableOpacity>
+        
+        <TouchableOpacity onPress={() => login(username, password)} className="w-11/12 h-14 flex justify-center bg-primary rounded-md items-center mt-8">
+          <Text className="text-white text-xl font-medium">Login</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View className="flex h-1/3 w-full justify-end items-center">
+        <TouchableOpacity onPress={() => login(username, password)} className="flex flex-row gap-1 mb-10">
+          <Text>Dont have an account?</Text>
+          <Text className="font-medium text-primary">Sign Up!</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
 }
 
-function Header() {
-  const { top } = useSafeAreaInsets();
-  return (
-    <View style={{ paddingTop: top }}>
-      <View className="px-4 lg:px-6 h-14 flex items-center flex-row justify-between ">
-        <Link className="font-bold flex-1 items-center justify-center" href="/">
-          ACME
-        </Link>
-        <View className="flex flex-row gap-4 sm:gap-6">
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            About
-          </Link>
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            Product
-          </Link>
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            Pricing
-          </Link>
-        </View>
-      </View>
-    </View>
-  );
-}
+const login = (username, password) => {
 
-function Footer() {
-  const { bottom } = useSafeAreaInsets();
-  return (
-    <View
-      className="flex shrink-0 bg-gray-100 native:hidden"
-      style={{ paddingBottom: bottom }}
-    >
-      <View className="py-6 flex-1 items-start px-4 md:px-6 ">
-        <Text className={"text-center text-gray-700"}>
-          © {new Date().getFullYear()} Me
-        </Text>
-      </View>
-    </View>
-  );
+}
+const resetPassword = (username, phoneNumber) => {
+
+}
+const createAccount = () => {
+
 }
--- Expo/app/tailwind.config.js
+++ Expo/app/tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text': "#06031f",
+        'background': "#f4f4fe",
+        'primary': "#5147c2",
+        'secondary': "#d8d89f",
+        'accent': "#ed7e58",
+        
+      },      
+    },
   },
   future: {
     hoverOnlyWhenSupported: true,
